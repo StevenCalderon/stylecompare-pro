9f9ae1098038e0721342a071a5b7d3fab2f691ec
Steven Calderon <steven.calderon@formos.com>
2024-06-27 06:17:07 -0500

    [Week-1] - Insert buttons in html

--- src/Popup/Popup.tsx
+++ src/Popup/Popup.tsx
@@ -1,84 +1,25 @@
-import { useState } from 'react';
-import './Popup.css';
+import { useEffect, useState } from 'react';
+import './PopupStyle.css';
 
 const Popup = () => {
-	const [element1, setElement1] = useState<{ [key: string]: string } | null>(
-		null
-	);
-	const [element2, setElement2] = useState<{ [key: string]: string } | null>(
-		null
-	);
+	const [isActive, setIsActive] = useState(false);
 
-	/*chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-		if (request.action === 'setElemetSelected' && request.styles) {
-			console.log(
-				'Received message: setElemetSelected',
-				request.elementNumber,
-				request
-			);
-			if (request.elementNumber === 1) {
-				setElement1(request.styles);
-			} else {
-				setElement2(request.styles);
-			}
-		}
-	});
-
-	const selectElement = async (elementNumber: number) => {
-		try {
-			const [tab] = await chrome.tabs.query({
-				active: true,
-				currentWindow: true,
-			});
-			if (tab.id !== undefined) {
-				chrome.scripting.executeScript({
-					target: { tabId: tab.id },
-					func: (elementNumber) => {
-						chrome.runtime.sendMessage(
-							{ action: 'selectElement', elementNumber: elementNumber },
-							(response) => {
-								console.log(
-									'Response from selectElement -- content.js:',
-									response
-								);
-							}
-						);
-					},
-					args: [elementNumber], //
-				});
-			}
-		} catch (error) {
-			console.error('Error querying tabs:', error);
-		}
-	};*/
-
-	/*const compareStyles = () => {
-		console.log('Elementos seleccionados', element1, element2);
-		if (!element1 || !element2) {
-			alert('Please select both elements first.');
-			return;
-		}
-
-		const differences: string[] = [];
-		for (let style in element1) {
-			if (element1[style] !== element2[style]) {
-				differences.push(`${style}: ${element1[style]} != ${element2[style]}`);
-			}
-		}
-
-		alert(
-			differences.length > 0 ? differences.join('\n') : 'No differences found.'
-		);
-	};*/
+	useEffect(() => {
+		chrome.storage.local.get(['activeExtension'], (result) => {
+			setIsActive(result.activeExtension || false);
+		});
+	}, []);
 
 	const changeCheckBox = (e: any) => {
-		chrome.storage.local.set({ activeExtension: e.target.checked });
+		const checked = e.target.checked;
+		setIsActive(checked);
+		chrome.storage.local.set({ activeExtension: checked });
 	};
 
 	return (
 		<div>
 			<label className="switch">
-				<input type="checkbox" onClick={(e) => changeCheckBox(e)} />
+				<input type="checkbox" checked={isActive} onChange={changeCheckBox} />
 				<span className="slider round"></span>
 			</label>
 		</div>
--- src/content.ts
+++ src/content.ts
@@ -1,94 +1,52 @@
 // contentScript.js
-export {};
-interface StorageChange {
-	oldValue?: any;
-	newValue?: any;
-}
-
-interface Changes {
-	[key: string]: StorageChange;
-}
-let listenerAttached = false;
-function addElementSelector(elementNumber: number) {
-	if (listenerAttached) {
-		return;
-	}
-	// Create a highlighter div
-	const highlighter = document.createElement('div');
-	highlighter.style.position = 'absolute';
-	highlighter.style.border = '2px dashed red';
-	highlighter.style.pointerEvents = 'none';
-	highlighter.style.zIndex = '10000';
-	document.body.appendChild(highlighter);
-
-	function onMouseMove(event: { target: any }) {
-		const target = event.target;
-		const rect = target.getBoundingClientRect();
-		highlighter.style.width = rect.width + 'px';
-		highlighter.style.height = rect.height + 'px';
-		highlighter.style.top = rect.top + 'px';
-		highlighter.style.left = rect.left + 'px';
-	}
-
-	function onClick(event: MouseEvent) {
-		event.preventDefault();
-		event.stopPropagation();
-		document.removeEventListener('mousemove', onMouseMove);
-		document.removeEventListener('click', onClick, true);
-		document.body.removeChild(highlighter);
-
-		const target = event.target as Element;
-		const styles = window.getComputedStyle(target);
-		const stylesObj: { [key: string]: string } = {};
-		for (const element of styles) {
-			stylesObj[element] = styles.getPropertyValue(element);
-		}
-
-		listenerAttached = false;
-		chrome.runtime.sendMessage({
-			action: 'setElemetSelected',
-			styles: stylesObj,
-			elementNumber: elementNumber,
-		});
-	}
-
-	document.addEventListener('mousemove', onMouseMove);
-	document.addEventListener('click', onClick, true);
-	listenerAttached = true;
-}
-
-// Listen for messages from the popup
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-	console.info('CONTENT --> ', request);
-	if (request.action === 'fetchStyles') {
-		addElementSelector(request.elementNumber);
-	}
-});
 
 chrome.storage.onChanged.addListener(
 	(
 		changes: { [key: string]: chrome.storage.StorageChange },
 		namespace: string
 	) => {
+		console.log('content -->', changes, namespace);
 		if (namespace === 'local' && changes.activeExtension) {
 			const activeExtension = changes.activeExtension.newValue;
 			if (activeExtension) {
 				// Si la extensión está activada, inyecta el botón "Select Element 1"
 				let button = document.getElementById(
-					'select-element-1'
+					'styleComparePro-select-element-1'
 				) as HTMLButtonElement;
 				if (!button) {
 					button = document.createElement('button');
 					button.innerText = 'Select Element 1';
-					button.id = 'select-element-1'; // Asegúrate de darle un ID único
+					button.id = 'styleComparePro-select-element-1'; // Asegúrate de darle un ID único
+					button.style.position = 'fixed';
+					button.style.top = '10px';
+					button.style.right = '10px';
+					button.style.zIndex = '10000'; // Asegúrate de que el botón esté en un nivel superior
+					button.style.padding = '10px';
+					button.style.backgroundColor = '#007bff';
+					button.style.color = 'white';
+					button.style.border = 'none';
+					button.style.borderRadius = '5px';
+					button.style.cursor = 'pointer';
+					let element1Selected = false;
 					button.addEventListener('click', () => {
-						// Lógica para seleccionar el primer elemento
+						if (!element1Selected) {
+							// Lógica para seleccionar el primer elemento
+							alert('Seleccionado Elemento 1');
+							element1Selected = true;
+							button.innerText = 'Select Element 2';
+						} else {
+							// Lógica para seleccionar el segundo elemento
+							alert('Seleccionado Elemento 2');
+							// Lógica adicional para comparar estilos
+						}
 					});
 					document.body.appendChild(button);
 				}
 			} else {
 				// Si la extensión está desactivada, remueve el botón "Select Element 1" si existe
-				const existingButton = document.getElementById('select-element-1');
+				const existingButton = document.getElementById(
+					'styleComparePro-select-element-1'
+				);
 				if (existingButton) {
 					existingButton.remove();
 				}
